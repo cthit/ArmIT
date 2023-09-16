3e75d9db75768f9cb5473a862bd5449046574670
Mikael Motin <motin@chalmers.se>
2023-09-16 18:29:30 +0200

    first commit

---
+++ armit_website/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    "Javascript/SvelteKIT code here"
+</script>
+
+
+<div class="w-full h-3/4 text-center items-center">
+    <div class="flex w-full justify-center items-center py-8">
+        <img class="h-1/3 max-h-72" alt="ArmIT Logo" src="/ArmITLogga.png"  />
+    </div>
+
+    <h1 class="text-3xl font-bold "> Welcome to ArmIT </h1>
+    <p class=" text-sm font-semibold">This site is under construction 🛠️</p>
+    <p>In the meantime.. check this out <a class="underline" href="https://chalmers.it">link</a> </p>
+</div>  
+
+
+
+
+  <style lang="postcss">
+    :global(html) {
+        @apply bg-blue-200;
+    }
+  </style>
