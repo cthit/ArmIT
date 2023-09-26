a89cc4500efd62641bfc3b9f50a1d302968afeb3
Lee4-M <liamn.mayor@gmail.com>
2023-09-26 22:18:33 +0200

    Home page changes

--- armit_website/src/routes/home/+page.svelte
+++ armit_website/src/routes/home/+page.svelte
@@ -12,14 +12,14 @@
         </div>
     </nav>
     <header class="grid grid-flow-col grid-rows-3 h-full font-inter text-white">
-        <div class="flex flex-col justify-center gap-8 w-full row-span-2 col-span-1 pl-20">
+        <div class="flex flex-col justify-center gap-8 w-full row-span-2 col-span-1 pl-20 pt-40">
             <div class="flex flex-col gap-4 2xl:text-5xl lg:text-3xl">
                 Chalmers' IT Corporate Relations Committee
                 <div class="w-3/5 2xl:text-2xl lg:text-xl">
                     Building meaningful connections that bridge the corporate-student divide
                 </div>
             </div>
-            <a href="/" class="flex justify-center px-8 py-3 w-1/4 2xl:text-lg md:text-base leading-none border-2 border-white lg:mt-0">Get in touch</a>
+            <a href="/" class="flex justify-center px-8 py-3 w-1/4 2xl:text-lg md:text-base leading-none border-2 border-white lg:mt-0">Our services</a>
         </div>
         <div class="flex flex-col gap-8 justify-start pl-20 w-full col-span-1 overflow-hidden 2xl:text-2xl lg:text-xl">
             Trusted by 
