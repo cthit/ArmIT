af9513558963bf34a029c8fd180d794e25095e8e
Lee4-M <liamn.mayor@gmail.com>
2023-09-27 17:13:32 +0200

    Alt home page

---
+++ armit_website/src/routes/home2/+page.svelte
@@ -0,0 +1,41 @@
+<div class="flex flex-col h-screen max-w-full">
+    <nav class="flex w-full h-28 bg-white text-navtext">
+        <div class="flex basis-1/2 justify-start items-center p-12 text-xl m-0 font-inter">
+            <a href="/" class="flex items-center">
+                <img class="h-24" alt="ArmIT Logo" src="/ArmITLogo_plain.png"/>
+            </a>
+        </div>
+        <!-- <div class="flex flex-grow justify-center gap-40 p-12 items-center text-xl m-0 font-inter md:visible invisible">
+            <div>Services</div>
+            <div>Team</div>
+            <div>DatE-IT</div>
+        </div> -->
+        <div class="flex flex-grow justify-end p-12 gap-20 items-center text-xl m-0 font-inter md:visible invisible">
+            <div>Services</div>
+            <div>Team</div>
+            <div>DatE-IT</div>
+            <a href="/" class="px-8 py-3 leading-none bg-navtext text-white lg:mt-0">Contact</a>
+        </div>
+    </nav>
+    <header class="flex flex-col bg-header justify-center gap-10 bg-no-repeat bg-cover md:object-scale-down h-full font-inter text-white">
+        <div class="flex flex-col justify-center gap-8 w-1/2 pl-40 pb-20">
+            <div class="flex flex-col gap-4 2xl:text-7xl lg:text-3xl">
+                Chalmers' IT Corporate Relations Committee
+                <div class="w-3/5 2xl:text-2xl lg:text-xl">
+                    Building meaningful connections that bridge the corporate-student divide
+                </div>
+            </div>
+            <a href="/" class="flex justify-center px-8 py-3 w-1/4 2xl:text-xl md:text-base leading-none bg-white text-navtext lg:mt-0">Our services</a>
+        </div>
+        <!-- <div class="flex flex-col gap-2 justify-start pl-40 w-2/3 text-navtext overflow-hidden 2xl:text-2xl lg:text-xl">
+            Trusted by 
+            <div class="flex gap-12">
+                <img class="w-1/6 max-h-full" alt="Centiro" src="/Centiro_logo.png"/>
+                <img class="w-1/6 max-h-full" alt="Opera" src="/Opera_GX_logo.png"/>
+                <img class="w-1/6 max-h-full pb-4" alt="Accenture" src="/Accenture_logo.png"/>
+            </div>
+        </div> -->
+    </header>
+</div>
+
+
