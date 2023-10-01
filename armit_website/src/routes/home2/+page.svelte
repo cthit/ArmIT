d9ce45fe78c3488e97aac65cd7d25b0f0bf47bb7
Lee4-M <104949387+Lee4-M@users.noreply.github.com>
2023-10-01 20:16:18 +0200

    Added menu to nav

--- armit_website/src/routes/home2/+page.svelte
+++ armit_website/src/routes/home2/+page.svelte
@@ -1,5 +1,5 @@
-<div class="flex h-screen w-full max-w-full max-h-full md:overflow-auto overflow-hidden">
-    <div class="flex flex-col w-full h-full bg-no-repeat bg-cover md:shrink-0 lg:bg-header bg-header-mobile">
+<div class="flex flex-col h-auto w-full max-w-full max-h-full">
+    <div class="flex flex-col w-full h-screen bg-no-repeat bg-cover lg:bg-header bg-header-mobile overflow-hidden">
         <nav class="flex w-full h-32 text-white">
             <div class="flex basis-1/2 justify-start items-center p-12 text-xl m-0 font-inter">
                 <a href="/" class="flex shrink-0 items-center">
@@ -31,12 +31,40 @@
                         <a href="/" class="flex justify-center px-8 py-3 w-1/3 md:visible invisible leading-none drop-shadow-xl bg-white text-navtext lg:mt-0">Our services</a>
                     </div>
                 </div>
-                <div class="flex flex-col flex-grow justify-center lg:justify-end gap-2 lg:text-start text-center lg:pl-40 mb-4 w-2/3 text-navtext overflow-hidden 2xl:text-2xl lg:text-xl">
+                <div class="flex flex-col flex-grow justify-center lg:justify-end gap-2 lg:text-start text-center lg:pl-40 mb-4 w-full px-4 text-navtext overflow-hidden 2xl:text-2xl lg:text-xl">
                     Trusted by 
                     <div class="flex items-center lg:justify-start justify-center w-full gap-12">
                         <img class="h-auto w-1/3 md:w-1/5 max-h-full" alt="Centiro" src="/Centiro_logo.png"/>
                         <img class="h-auto w-1/3 md:w-1/5 max-h-full" alt="Opera" src="/Opera_GX_logo.png"/>
                         <img class="h-auto w-1/3 md:w-1/5 max-h-full pb-4" alt="Accenture" src="/Accenture_logo.png"/>
+                    </div>
+                </div> 
+            </div>
+        </header>
+    </div>
+    <div class="lg:hidden flex flex-col w-full h-screen bg-white overflow-hidden">
+        <nav class="flex w-full h-32 text-white">
+            <div class="flex w-full justify-end p-12 items-center">
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="#1a2d45" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                </button>
+            </div>
+        </nav>
+        <header class="flex h-full font-inter text-navtext">
+            <div class="flex flex-col h-full lg:items-start items-center justify-center">
+                <div class="flex flex-grow justify-end flex-col gap-8 w-full pl-10">
+                    <a href="#" class="flex text-4xl">
+                        Contact us
+                    </a>
+                    <a href="#" class="flex text-4xl">
+                        DatE-IT
+                    </a>
+                </div>
+                <div class="flex flex-col flex-grow justify-end w-full text-navtext overflow-hidden">
+                    <div class="flex items-center justify-center w-full gap-8 mb-8">
+                        <i class="fa-brands fa-facebook fa-xl"></i>
+                        <i class="fa-brands fa-instagram fa-xl"></i>
+                        <i class="fa-brands fa-linkedin fa-xl"></i>
                     </div>
                 </div> 
             </div>
