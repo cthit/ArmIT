ba5bf32379d9977fb96d6f9844f5046da3f85751
mikaelmotin <motin@chalmers.se>
2023-10-02 21:47:53 +0200

    updates in Cards

--- armit_website/src/components/Cards.svelte
+++ armit_website/src/components/Cards.svelte
@@ -1,10 +1,13 @@
 <!-- These cards will be displayed on the main page -->
 
 <script>
-    
+    import {getProductcatalogue} from "../scripts/getProductcatalogue"
 </script>
 
+{#await getProductcatalogue("en")}
+<div class="flex h-screen w-screen items-center justify-center text-center font-semibold text-3xl">Loading Content...</div>
 
+{:then data}
 <section class="mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 justify-items-center justify-center gap-y-10 md:gap-y-8 lg:gap-y-10 gap-x-4 md:gap-x-8 lg:gap-x-10 max-w-screen-xl mt-10 mb-5">   
     <!-- First Card -->
     <div class="relative text-clip w-fit max-w-[18rem] bg-[#1F43FF] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
@@ -13,12 +16,12 @@
                 <img src="/circle.png" alt="" class="h-full"/>
             </div>
             
-            <img draggable="false"  src="/teacher_whiteboard.png" alt="teacher with whiteboard " class="relative h-42 object-scale-down rounded-t-xl mx-auto">
+            <img draggable="false"  src="{data.products[0].iconPath}" alt="teacher with whiteboard " class="relative h-42 object-scale-down rounded-t-xl mx-auto">
         
             <div class="relative px-4 w-fit max-w-[18rem] mb-2">
-                <p class="text-lg font-bold text-white truncate block capitalize">Product Name</p>
+                <p class="text-lg font-bold text-white truncate block capitalize">{data.products[0].itemName}</p>
                 <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm">We offer both digital and physical seminars where you can capture your audience! We offer both digital and physical seminars where you can capture your audience!</p>
+                    <p class="text-white text-base font-semibold drop-shadow-sm">{data.products[0].description}</p>
                 </div>
             </div>
         </a>
@@ -126,7 +129,7 @@
             <div class="relative px-4 w-fit max-w-[18rem] mb-2">
                 <p class="text-lg font-bold text-white truncate block capitalize">Product Name</p>
                 <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibol">We offer both digital and physical seminars where you can capture your audience! We offer both digital and physical seminars where you can capture your audience!</p>
+                    <p class="text-white text-base font-semibold">We offer both digital and physical seminars where you can capture your audience! We offer both digital and physical seminars where you can capture your audience!</p>
                 </div>
             </div>
         </a>
@@ -135,4 +138,4 @@
     
 </section>
 
-
+{/await}
