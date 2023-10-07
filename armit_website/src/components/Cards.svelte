562abc3f883ab73abd3838610fbc8af5fefbfced
Mikael Motin <motin@chalmers.se>
2023-10-07 21:25:55 +0200

    tweaked responsiveness of cards

--- armit_website/src/components/Cards.svelte
+++ armit_website/src/components/Cards.svelte
@@ -8,160 +8,194 @@
 <div class="flex h-screen w-screen items-center justify-center text-center font-semibold text-3xl">Loading Content...</div>
 
 {:then data}
-<section class="mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 justify-items-center justify-center gap-y-10 md:gap-y-8 lg:gap-y-10 gap-x-4 md:gap-x-8 lg:gap-x-10 max-w-screen-xl mt-10 mb-5">   
+<!-- Remove below h1 later -->
+<h1 class="text-black mt-20 text-center w-screen font-bold text-4xl mb-8">Explore Our Products</h1>
+
+<section class="mx-auto justify-center justify-items-center 
+                grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4   
+                md:max-w-[800px] lg:max-w-[1000px] 2xl:max-w-[1240px]
+                gap-x-[50px] gap-y-[50px]">
     
-    <!-- First Card - Lunch Seminars -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#1F43FF] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
+    <!-- First Card -->
+    <div class="relative bg-[#1F43FF] overflow-hidden rounded-3xl
+                w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+                duration-500 hover:scale-110 hover:shadow-xl">
+        
         <a href="/" draggable="false" class="flex-1">
             <div class="z-0 absolute bottom-0">
-                <img src="/circle.png" alt="" class="h-full"/>
+                <img src="/circle.png" alt="" class="max-w-full"/>
             </div>
-            
-            <img draggable="false"  src="{data.products[0].iconPath}" alt="teacher with whiteboard " class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].description}</p>
+
+            <img draggable="false"  src="{data.products[0].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+            <div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+                        w-full h-fit">
+                <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].itemName}</p>
+                
+                <div class="text-left items-center line-clamp-4">
+                    <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].description}</p>
                 </div>
             </div>
         </a>
     </div>
 
-    <!-- Second Card - Hackatons -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#00FFC2] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
-        <a href="/" draggable="false" class="flex-1">
-            <div class="z-0 absolute bottom-0 w-full">
-                <img src="/bg_hackatons.png" alt="" class=" h-40 w-full"/>
-            </div>
-            
-            <img draggable="false"  src="{data.products[1].iconPath}" alt="</>" class="relative h-42 object-scale-down rounded-t-xl mx-auto">
+    <!-- Second Card -->
+    <div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+                w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+                duration-500 hover:scale-110 hover:shadow-xl">
         
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2 pb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
-                </div>
-            </div>
-        </a>
-    </div>
-
-    <!-- Third Card - Not Decided -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#361497] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
-        <a href="/" draggable="false" class="flex-1">
-            <div class="z-0 absolute bottom-0 w-full">
-                <img src="" alt="" class=" h-40 w-full"/>
-            </div>
-            
-            <img draggable="false"  src="{data.products[2].iconPath}" alt="ALT FOR IMAGE" class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2 pb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[2].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[2].description}</p>
-                </div>
-            </div>
-        </a>
-    </div>
-
-    <!-- Forth Card - Not Decided -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#EB8628] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
-        <a href="/" draggable="false" class="flex-1">
-            <div class="z-0 absolute bottom-0 w-full">
-                <img src="" alt="" class=" h-40 w-full"/>
-            </div>
-            
-            <img draggable="false"  src="{data.products[2].iconPath}" alt="ALT FOR IMAGE" class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2 pb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[2].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[2].description}</p>
-                </div>
-            </div>
-        </a>
-    </div>
-
-    <!-- Fifth Card - Not Decided -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#BB4DFF] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
         <a href="/" draggable="false" class="flex-1">
             <div class="z-0 absolute bottom-0">
-                <img src="/circle.png" alt="" class="h-full"/>
+                <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
             </div>
-            
-            <img draggable="false"  src="{data.products[0].iconPath}" alt="teacher with whiteboard " class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].description}</p>
+
+            <img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+            <div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+                        w-full h-fit">
+                <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+                
+                <div class="text-left items-center line-clamp-4">
+                    <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
                 </div>
             </div>
         </a>
     </div>
 
-    <!-- Sixth Card - Not Decided -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#FFFA87] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
+    
+    <!-- Placeholders below -->
+    <div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+                w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+                duration-500 hover:scale-110 hover:shadow-xl">
+        
         <a href="/" draggable="false" class="flex-1">
             <div class="z-0 absolute bottom-0">
-                <img src="/circle.png" alt="" class="h-full"/>
+                <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
             </div>
-            
-            <img draggable="false"  src="{data.products[0].iconPath}" alt="teacher with whiteboard " class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[0].description}</p>
+
+            <img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+            <div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+                        w-full h-fit">
+                <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+                
+                <div class="text-left items-center line-clamp-4">
+                    <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
                 </div>
             </div>
         </a>
     </div>
+    <div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+    w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+    duration-500 hover:scale-110 hover:shadow-xl">
 
-    <!-- Seventh Card - Not Decided -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#463C4B] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
+<a href="/" draggable="false" class="flex-1">
+<div class="z-0 absolute bottom-0">
+    <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
+</div>
+
+<img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+<div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+            w-full h-fit">
+    <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+    
+    <div class="text-left items-center line-clamp-4">
+        <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
+    </div>
+</div>
+</a>
+</div>
+<div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+                w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+                duration-500 hover:scale-110 hover:shadow-xl">
+        
         <a href="/" draggable="false" class="flex-1">
-            <div class="z-0 absolute bottom-0 w-full">
-                <img src="" alt="" class=" h-40 w-full"/>
+            <div class="z-0 absolute bottom-0">
+                <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
             </div>
-            
-            <img draggable="false"  src="{data.products[1].iconPath}" alt="</>" class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2 pb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
+
+            <img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+            <div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+                        w-full h-fit">
+                <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+                
+                <div class="text-left items-center line-clamp-4">
+                    <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
                 </div>
             </div>
         </a>
     </div>
+    <div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+    w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+    duration-500 hover:scale-110 hover:shadow-xl">
 
-    <!-- Eight Card - Not Decided -->
-    <div class="relative text-clip w-fit max-w-[18rem] bg-[#FF6262] rounded-3xl duration-500 hover:scale-105 hover:shadow-xl ">
+<a href="/" draggable="false" class="flex-1">
+<div class="z-0 absolute bottom-0">
+    <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
+</div>
+
+<img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+<div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+            w-full h-fit">
+    <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+    
+    <div class="text-left items-center line-clamp-4">
+        <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
+    </div>
+</div>
+</a>
+</div>
+<div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+                w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+                duration-500 hover:scale-110 hover:shadow-xl md:hidden lg:block">
+        
         <a href="/" draggable="false" class="flex-1">
-            <div class="z-0 absolute bottom-0 w-full">
-                <img src="" alt="" class=" h-40 w-full"/>
+            <div class="z-0 absolute bottom-0">
+                <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
             </div>
-            
-            <img draggable="false"  src="{data.products[1].iconPath}" alt="</>" class="relative h-42 object-scale-down rounded-t-xl mx-auto">
-        
-            <div class="relative px-4 w-fit max-w-[18rem] mb-2 pb-2">
-                <p class="text-2xl font-bold text-white truncate block capitalize text-center pb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
-                <div class="items-center line-clamp-4">
-                    <p class="text-white text-base font-semibold drop-shadow-sm" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
+
+            <img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+            <div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+                        w-full h-fit">
+                <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+                
+                <div class="text-left items-center line-clamp-4">
+                    <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
                 </div>
             </div>
         </a>
     </div>
+    <div class="relative bg-[#00FFC2] overflow-hidden rounded-3xl
+    w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+    duration-500 hover:scale-110 hover:shadow-xl md:hidden lg:block">
+
+<a href="/" draggable="false" class="flex-1">
+<div class="z-0 absolute bottom-0">
+    <img src="/bg_hackatons.png" alt="" class="max-w-full"/>
+</div>
+
+<img draggable="false"  src="{data.products[1].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+<div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+            w-full h-fit">
+    <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].itemName}</p>
+    
+    <div class="text-left items-center line-clamp-4">
+        <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[1].description}</p>
+    </div>
+</div>
+</a>
+</div>
 
     
+</section> 
 
-    
-
-    
-    
-    
-    
-</section>
-
+<!-- Remove below button later, should not be in this class -->
+<div class="w-screen justify-items-center text-center font-semibold text-white shadow-cyan-400-2xl mb-20">
+    <button class="mt-8 w-64 h-14 bg-blue-700 duration-500 hover:bg-blue-800 hover:scale-105 rounded-full">See the Full Catalogue</button>
+</div>
 {/await}
