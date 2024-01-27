9df2762d20ebe62c43e13930982b160e4ffed384
Lee4-M <104949387+Lee4-M@users.noreply.github.com>
2024-01-27 01:46:43 +0100

    Refactoring HTML to components and added a store

--- armit_website/src/routes/home2/+page.svelte
+++ armit_website/src/routes/home2/+page.svelte
@@ -1,16 +1,25 @@
 <script>
     import Menu from "../../components/Menu.svelte"
     import MobileMenu from '../../components/MobileMenu.svelte';
+    import {menuToggler} from "../../store/menuStore"
+    import { onDestroy } from "svelte";
+    let visible = false;
+
+    const unsubscribe = menuToggler.subscribe(value => {
+        visible = value;
+    });
+
+    onDestroy(() => {
+        unsubscribe();
+    });
 </script>
 
 <div class="flex flex-col h-auto w-full max-w-full max-h-full">
     <div class="flex w-full h-screen relative">
         <Menu/>
-        
         {#if visible}
             <MobileMenu/>
-        {/if}
-        
+        {/if}  
     </div>
 </div>
 
