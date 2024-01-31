9953417bac3ca43e5edb90b844485478aef09380
mikaelmotin <motin@chalmers.se>
2024-01-31 23:05:16 +0100

    merch card

--- armit_website/src/components/Cards.svelte
+++ armit_website/src/components/Cards.svelte
@@ -106,6 +106,29 @@
         </a>
     </div>
 
+    <!-- Give Our Merch (5) -->
+    <div class="relative bg-[#BB4DFF] overflow-hidden rounded-3xl
+    w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
+    duration-500 hover:scale-110 hover:shadow-xl">
+    
+        <a href="/" draggable="false" class="flex-1">
+            <div class="z-0 absolute bottom-0 w-full">
+                <img src="/sponsored_bg.png" alt="" class="max-w-full w-full object-cover"/>
+            </div>
+
+            <img draggable="false"  src="{data.products[5].iconPath}" alt="teacher with whiteboard " class="relative px-4 mx-auto">
+
+            <div class="relative text-center mx-auto px-4 mb-2 -mt-4 
+                        w-full h-fit">
+                <p class="text-xl 2xl:text-2xl font-semibold text-white truncate mb-1" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[5].itemName}</p>
+                
+                <div class="text-left items-center line-clamp-4">
+                    <p class="text-xs 2xl:text-base font-semibold text-white" style="text-shadow: 2px 4px 3px rgba(0,0,0,0.3);">{data.products[5].description}</p>
+                </div>
+            </div>
+        </a>
+    </div>
+
     <!-- Study Breakfast 6th -->
     <div class="relative bg-[#FFFA87] overflow-hidden rounded-3xl
     w-full h-full max-w-[200px] max-h-[240px] 2xl:min-w-[240px] 2xl:min-h-[300px] 2xl:max-w-[300px] 2xl:max-h-[360px]
