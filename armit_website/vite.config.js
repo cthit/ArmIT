f328ca3d7f9c4626d5ebace5c127919c92c98947
Lee4-M <liamn.mayor@gmail.com>
2024-02-07 04:31:22 +0100

    holy fuck it works

--- armit_website/vite.config.js
+++ armit_website/vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
         host: true,
-        port: 8080
+        port: 3000
+    },
+    preview: {
+        host: true,
+        port: 3000
     }
 });
