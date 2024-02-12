dc25192730447022dc44fd72bff49c1f9cac7cc2
mikaelmotin <motin@chalmers.se>
2024-02-12 21:26:11 +0100

    fixed outline

--- armit_website/src/components/Partners.svelte
+++ armit_website/src/components/Partners.svelte
@@ -21,16 +21,16 @@
     </div>
     <div class="flex flex-col gap-10 justify-center text-center items-center text-white w-full">
         <div class="text-5xl lg:text-7xl font-inter font-bold">Want to get in touch?</div>
-            <form method="POST" id="contactMeForm" class="xl:grid hidden grid-cols-2 gap-4 rounded-3xl border-2 border-black h-14 w-1/4">
+            <form method="POST" id="contactMeForm" class="xl:flex flex-row hidden  gap-4 rounded-full border-2 border-black h-14">
                 <div class="flex justify-center items-center pl-4 text-[#979797]">
-                    <input required class="bg-transparent text-center" placeholder="email@example.com" type="email" id="e-mail" name="e-mail"><br><br>
+                    <input required class="bg-transparent focus:text-white text-center rounded-full outline-none" placeholder="email@example.com" type="email" id="e-mail" name="e-mail"><br><br>
                 </div>
-                <div class="flex gap-2 justify-center hover:opacity-80 transition cursor-pointer items-center rounded-3xl bg-[#6BBBF8] border-black border-2 m-1 text-white shadow-lg">
+                <div class="flex gap-2 px-8 justify-center hover:opacity-80 transition cursor-pointer items-center rounded-3xl bg-[#6BBBF8] border-black border-2 m-1 text-white shadow-lg">
                     <input type="submit" value="Contact me" class="cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                 </div>
             </form>
-        <a href="/contact" class="flex gap-2 w-48 h-14 justify-center hover:opacity-80 hover:scale-110 transition items-center rounded-3xl text-xl bg-[#196AB6] border-black border-2 m-1 text-white shadow-lg" draggable="false">
+        <a href="/contact" class="flex gap-2 w-48 h-14 justify-center hover:opacity-80 hover:scale-110 transition items-center rounded-full text-xl bg-[#196AB6] border-black border-2 m-1 text-white shadow-lg" draggable="false">
             Contact us
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
         </a>
