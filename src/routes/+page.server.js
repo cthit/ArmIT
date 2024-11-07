a8a2a94a95f94df4b1c53a433e75b855b6020c3e
Mikael Motin <motin@chalmers.se>
2024-11-07 11:06:35 +0100

    use GOTIFY (#6)
    * tst
    
    * updated to gotify for contact forms
    
    * clean-up
    
    * clean-up
    
    * Use dynamic ENV
    
    * Use only ArmIT email and not personal
    
    ---------
    
    Co-authored-by: Oscar Eriksson <oscariremma@gmail.com>

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,39 +1,41 @@
 // src/routes/contact/+page.server.js
-import nodemailer from 'nodemailer';
+import { env } from '$env/dynamic/private'
 
 export const actions = {
     default: async ({ request }) => {
         const form = await request.formData();
         const email = form.get('e-mail');
 
-        let transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: 'motin@chalmers.it',
-                pass: 'bcsvlrwvobdfacwg'
-            }
-        });
-
+        // Can change this in the future if u want:
         let mailOptions = {
-            from: '"Armit (Contact ME)" <motin@chalmers.it>',
-            to: 'armit@chalmers.it, motin@chalmers.it',
+            from: '"Armit (Contact ME)" <armit@chalmers.it>',
+            to: 'armit@chalmers.it',
             subject: '💃💃Contact ME Form',
             text: "Please reach out to me at: " + email
         };
 
         try {
-            await transporter.sendMail(mailOptions);
+            const info = await fetch(env.GOTIFY_URL, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'pre-shared: ' + env.GOTIFY_TOKEN
+                },
+                body: JSON.stringify(mailOptions)
+              });
 
-            return {
-                status: 200 
-            };
+            console.log('Message sent: %s', info.messageId);
+
         } catch (error) {
-            console.error('Failed to send email:', error);
-
+            console.error('Error occurred while sending email:', error);
             return {
                 status: 500,
                 errors: { message: 'Failed to send email' }
             };
         }
+        return {
+            status: 200
+        };
+
     }
 };
--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,9 +1,7 @@
 import { fail } from "@sveltejs/kit";
 import { z } from "zod";
 import { superValidate, message } from "sveltekit-superforms/server";
-import nodemailer from 'nodemailer';
-
-
+import { env } from '$env/dynamic/private'
 
 const newContactSchema = z.object({
     company: z.string().min(1),
@@ -14,77 +12,43 @@ const newContactSchema = z.object({
 });
 
 export const actions = {
-    // default: async ({ request }) => {
-    //   const form = await superValidate(request, newContactSchema);
-    //   console.log('POST', form);
-  
-    //   // Convenient validation check:
-    //   if (!form.valid) {
-    //     // Again, return { form } and things will just work.
-    //     return fail(400, { form });
-    //   }
-  
-    //   // TODO: Do something with the validated form.data
-  
-    //   // Yep, return { form } here too
-    //   return { form };
-    // },
     sendEmail: async ({ request }) => {
         const form = await superValidate(request, newContactSchema);
         console.log(form)
-        
-        
         if (!form.valid) {
-        // Again, return { form } and things will just work.
+        
         return fail(400, { form });
         }
 
-        // TODO - send email:
+        // - send email:
         try {
-            // Create a Nodemailer transporter using SMTP
-            let transporter = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: 'motin@chalmers.it', 
-                    pass: 'bcsvlrwvobdfacwg' 
-                }
-            });
-    
             // Define the email message
             let mailOptions = {
-                from: '"Armit Website Contact Form" motin@chalmers.it', // sender address
-                to: 'armit@chalmers.it, motin@chalmers.it', // list of receivers
+                to: 'armit@chalmers.it', // list of receivers
+                from: '"ArmIT Website Contact Form" armit@chalmers.it', // sender address
                 subject: '$CASH SEASON! 💵🤑ArmIT Contact Form Proposal', // Subject line
-                text: "Company: " + form.data.company + "\n" +
+                body: "Company: " + form.data.company + "\n" +
                     "Name: " + form.data.name + "\n" +
                     "Email: " + form.data.email + "\n" +
                     "Phone: " + form.data.phone + "\n" +
                     "Message: " + form.data.message // plain text body
             };
-    
+
             // Send mail with defined transport object
-            let info = await transporter.sendMail(mailOptions);
-    
+            const info = await fetch(env.GOTIFY_URL, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'pre-shared: ' + env.GOTIFY_TOKEN
+                },
+                body: JSON.stringify(mailOptions)
+              });
+
             console.log('Message sent: %s', info.messageId);
         } catch (error) {
             console.error('Error occurred while sending email:', error);
         }
-        
+
         return message(form, "Thanks for reaching out!" );
     }
   };
-
-
-
-
-
-
-// /** @type {import('./$types').Actions} */
-// export const actions = {
-// 	sendEmail: async ({ request }) => {
-//         const data = await request.formData();
-//         const email = data.get('email');
-
-// 		console.log("Email is being delivered" + email)
-// 	}
-// };
